1a39fb25deb72a3d6e0077a7635f7be80bff4516
JovanPetkovic99 <jovanpetkovicdevelopment@gmail.com>
2022-10-23 14:32:29 +0200

    Finished first task

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>NBSoft - Jovan Petkovic</title>
+    <link rel="stylesheet" href="public/css/html.css">
+    <link href="/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <form action="modules/Task">
+            <button type="submit" value="html">Task 1</button>
+            <button type="submit" value="form">Task 2</button>
+            <button type="submit" value="slider">Task 3</button>
+            <button type="submit" value="folder">Task 4</button>
+            <button type="submit" value="sql">Task 5</button>
+        </form>
+</header>
+    <div id="container">
+        
+    </div>
+    <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ templates/html.php
@@ -0,0 +1,94 @@
+<div id="html">
+    <div id="contact-info">
+        <h1>Jovan Petković</h1>
+        <div>
+            <h2>jovanpetkovicdevelopment@gmail.com</h2>
+            <h2>+381652060042</h2>
+        </div>
+        <h2>Belgrade, Serbia<h2>
+    </div>
+    <div id="job-history">
+        <h2 class="section-title">WORK EXPERIENCE</h2>
+        <div>
+            <h3>Pyroland – Feniks Operativa</h3>
+            <h3>June 2021 – Present</h3>
+            <h3>Belgrade, Serbia</h3>
+            <p>Contract Work - Maintenance and Setup of Fire Protection Systems</p>
+            <ul>
+                <li>Pyroland is a construction firm focusing on Fire Protection Systems used in large buildings</li>
+                <li>Core Responsibility #1: Installment and maintenance of these systems in the University Clinical Centre of Belgrade</li>
+                <li>86000 square meters covered with Fire Protections Systems</li>
+                <li>Core Responsibility #2: Projecting the layout of the system with the overview of mechanical engineers</li>
+                <li>This job taught me responsibility, the value of cooperation with different crews and companies on the site, and
+                the importance of adherence and listening to the higher-ups.</li>
+            </ul>
+        </div>
+
+        <div>
+            <h3>Decathlon</h3>
+            <h3>June 2020 – December 2021</h3>
+            <h3>Belgrade, Serbia</h3>
+            <p>Part-Time Seller</p>
+            <ul>
+                <li>Core Responsibility #1: Talking with customers and answering all questions about the goods in the store.</li>
+                <li>Core Responsibility #2: Managing the layout of the hunting, soccer, basketball, and fishing sections of the store.</li>
+            </ul>
+
+        </div>
+        <div>
+            <h3>Youth Cooperative “Bulevar”</h3>
+            <h3>December 2018 – June 2021</h3>
+            <h3>Belgrade, Serbia</h3>
+            <p>Various Part-Time Jobs</p>
+            <ul>
+                <li>Worked in Gebruder Weiss, Aks Express, Bex Express, City Express, LCW Waikiki, OfferTrade, Moon Sushi & Fusion Food</li>
+            </ul>
+        </div>
+    </div>
+    <div id="education">
+
+        <h2 class="section-title">EDUCATION</h2>
+        <div>
+            <h3>The University of Belgrade, Faculty of Organisational Sciences</h3>
+            <h3>2018 – Present (Advanced University Student)</h3>
+            <h3><a href="www.fon.bg.ac.rs">www.fon.bg.ac.rs</a></h3>
+        </div>
+        <div>
+            <h3>Mathematical High School, Belgrade</h3>
+            <h3>2014 – 2018 (Graduated)</h3>
+            <h3><a href="www.mg.edu.rs">www.mg.edu.rs</a></h3>
+            <ul>
+                <li>Finished with an average grade of 5.0</li>
+                <li>President and representative of my class in the student parliament through all 4 years of schooling</li>
+            </ul>
+        </div>
+    </div>
+    <div>
+        <h2 class="section-title">SKILLS & INTERESTS</h2>
+        <ul>
+            <li>Skills: HTML, CSS, JavaScript, PHP, Laravel, OOP, Git, Bash, Linux, Photoshop, Googling,
+                Advanced Mathematics, Management, Marketing, Accounting, Fluent English in both speaking and writing,
+                Basic Knowledge of Java, C#, C++, Pascal, Prolog, and Assembly.</li>
+            <li>Interests: Volunteering, Hiking, Programming, Playing Guitar, Activism,</li>
+        </ul>
+    </div>
+
+    <div>
+        <h2 class="section-title">PROJECTS and COURSES</h2>
+        <ul>
+            <li><b>Blips & Chitz:</b> Internet store for cartoon, anime, and game artifacts, shirts, hoodies, etc.</li>
+            <li><b>Srpski Velikani:</b> Website on Serbian people of importance. Showing information about them in a visually pleasing way.</li>
+            <li><b>Wurdily:</b> Static site built for a university assignment.</li>
+            <li><b>VissionImmersion:</b> Redesigned and rebuilt static website.</li>
+        </ul>
+
+    </div>
+    <div>
+        <ul>
+            <li><b>Web Developer Bootcamp</b> – Colt Steele</li>
+            <li><b>Laravel 8 From Scratch</b> – Jeffrey Way</li>
+            <li><b>Learn Photoshop, Web Design & Profitable Freelancing</b> – Cristian Doru Barin</li>
+            <li><b>Youtube, Stack Overflow, and many other sites</b></li>
+        </ul>
+    </div>
+</div>
